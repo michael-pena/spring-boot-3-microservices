cc96f19c10b04cb20ff58c8e0f0e02b5b55dadc0
michael-pena <michael.pena@protonmail.ch>
2025-03-11 19:07:42 -0500

    added docker compose files for services, enabled actuators, added README files, added initial docker-compose file

--- customer-ms/src/main/java/com/mpena/customer_ms/CustomerMsApplication.java
+++ customer-ms/src/main/java/com/mpena/customer_ms/CustomerMsApplication.java
@@ -8,6 +8,12 @@ public class CustomerMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerMsApplication.class, args);
+		//TODO: add csv data
+		//TODO: add feign client
+		//TODO: add config server
+		//TODO: add eureka server
+		//TODO: add google jib dependency to both MS: run command to create container
+		//TODO: create docker compose file
 	}
 
 }

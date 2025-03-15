b289610a880ed095deafb1dac4e0fa087633c9dd
michael-pena <michael.pena@protonmail.ch>
2025-03-15 15:12:46 -0500

    added path rewrites on gateway server to include /bank as a prefix for service endpoints

--- gatewayserver/src/main/java/com/mpena/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/mpena/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.mpena.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,17 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator configRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+			.route(p -> p
+				.path("/bank/accounts/**")
+				.filters( f -> f.rewritePath("/bank/accounts/(?<segment>.*)","/${segment}"))
+				.uri("lb://ACCOUNTS"))
+			.route(p -> p
+				.path("/bank/customers/**")
+				.filters( f -> f.rewritePath("/bank/customers/(?<segment>.*)","/${segment}"))
+				.uri("lb://CUSTOMERS"))
+			.build();
+	}
 }
